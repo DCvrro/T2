986b6ae44e4842e2a3cd54b3a5cdb6035ef558eb
Diego Carrillo <diego.carrillo1@mail.udp.cl>
2023-05-11 22:42:59 -0400

    .
    .

--- p1.py
+++ p1.py
@@ -6,17 +6,29 @@ def leerArchivo():
     with open('t2_Deimos.txt','r+') as _file:
         file = _file.readlines()
         for linea in file:
-            if count == 0:
-                cant_uav = linea
-                uavs['cant'] = cant_uav
-                count = count + 1
             linea = linea.split(' ')
             if('\n' in linea[-1]):
                 if(linea[-1] == '\n'): 
                     linea.pop()
                 else:
                     linea[-1] = (linea[-1])[:-1]
-            print(uavs)
+            if count == 0:
+                cant_uav = linea
+                uavs['cant'] = cant_uav
+                count = count + 1
+            elif count == 1:
+                bestTime = linea[0]  ## Mejor Timepo
+                uavs['bestTime'] = bestTime
+
+                prefTime = linea[1]
+                uavs['prefTime'] = prefTime
+
+                slowTime = linea[-1]
+                uavs['slowTime'] = slowTime
+                
+
+
+        print(uavs)
 leerArchivo()
 
 ## Cantidad UAVS
