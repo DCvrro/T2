d6ad9175a26dab7334c28d58b7f755f89e74eb37
Abel <abel.baulloza@outlook.cl>
2023-05-12 22:16:10 -0400

    Estructura para los datos hecho.

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 def leer():
-    
-    with open('t2_Titan.txt','r+') as file:
+    drones = []
+    with open('t2_Europa.txt','r+') as file:
         count = 0
         uav = {}
-        drones = []
-
+        tiempos = []
+        times = []
         for lines in file.readlines():
             lines = lines.split(' ')
 
@@ -13,48 +13,58 @@ def leer():
                 if(lines[-1] == '\n'): 
                     lines.pop()
                 else:
-                    lines[-1] = (lines[-1])[:-1] ## corta los nuemeros o palabras con un \n
+                    lines[-1] = (lines[-1])[:-1] ## corta los números o palabras con un \n
             
-
             ##### Ingreso al diccionario #####
             if count == 0: ### Ingreso datos Cantidad de uavs
-                uav['cantidad'] = lines
-                temp = lines
+                uav['cantidad'] = int(lines[0])
+                limit = int(lines[0])
                 count = 1
                 continue
 
                 ### Ingreso informacion de tiempos
             if count == 1 and len(lines) == 3:
-                if('cantidad' not in uav):
-                    uav['cantidad'] = temp
-
-                uav['botTime'] = lines[0]
-                uav['midTime'] = lines[1]
-                uav['topTime'] = lines[-1]
+                uav = {
+                    'botTime':0,
+                    'midTime':0,
+                    'topTime':0,
+                    'times':[]
+                }
+                times = []
+                uav['botTime'] = int(lines[0])
+                uav['midTime'] = int(lines[1])
+                uav['topTime'] = int(lines[-1])
+                uav['times'] = times
+                
                 count = 2
                 continue
                 
+
                 ###Ingreso los tiempos asociados a la cantidad y de cada uav.
             if(count == 2 ):
                 if 'times' in uav: ## Reviso si ya existe la seccion de registros de tiempos en el diccionario
-                    if len(uav['times']) < int((uav.get('cantidad'))[0]): #Los voy ingresando si es que no cumplen con la cantidad dedatos dichos.
+                    if len(times) < limit: #Los voy ingresando si es que no cumplen con la cantidad dedatos dichos.
                         for datos in lines: 
-                            uav['times'].append(datos)
-                    else:
-                        count = 1  
-                        drones.append(uav)
-                        uav.clear()  
-                else: # en caso contrario los creo y empieza a ingresar dentro del diccionario
-                    for datos in lines:
-                        if 'times' in uav: 
-                            uav['times'].append(datos)
-                        else:
-                            uav['times'] = [datos]
-    print(drones) 
-    print((drones[0]).get('cantidad'))
-    for drone in drones: 
-        print('botTime:',(drone.get('botTime')))  
-        print('midTime:',(drone.get('midTime')))
-        print('topTime:',(drone.get('topTime')))
-        print('Tiempos:', drone.get('times')) 
+                            tiempos.append(int(datos))
+                        times.extend(tiempos)
+                        tiempos.clear()
+                        if len(times) == limit:
+                            #print('todos los tiempos')
+                            count = 1  
+                            uav['times'] = times
+                            print(uav)
+                            drones.append(uav)
+                            #times.clear()
+        #print(drones) 
+    #print((drones[0]).get('cantidad'))
+    #for drone in drones: 
+        #print('botTime:',(drone.get('botTime')))  
+        #print('midTime:',(drone.get('midTime')))
+        #print('topTime:',(drone.get('topTime')))
+        #print('Tiempos:', drone.get('times')) 
+    return drones
 leer()
+
+if __name__ == '__main__':
+    a = leer()
+    print(a)
